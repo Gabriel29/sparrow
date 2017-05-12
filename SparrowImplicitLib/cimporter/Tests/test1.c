42451ede2a54eeeb90dc2bcfaa54fedd055efc26
Gabriel Niculae <gabriel.niculae29@gmail.com>
2017-05-13 02:19:48 +0300

    CImporter draft

---
+++ SparrowImplicitLib/cimporter/Tests/test1.c
@@ -0,0 +1,18 @@
+int a, b;
+int i = 1 + 2;
+void *pointer;
+char **pointer2;
+
+struct my_struct {
+	int a;
+	int b;
+	char *p;
+} struct_ex;
+
+void foo();
+void foo2(int a, int b);
+
+void print_vector(int n, int **v);
+
+int test(int a);
+int* test2(int *a);
